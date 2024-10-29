9936c27afc67c23d0620c7b4ba46f1e1b0203531
angel <angelcomu04@gmail.com>
2024-10-29 17:29:28 +0100

    register hecho

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule, MatLabel, MatHint } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatLabel, MatHint, MatButtonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
