d8db45acbe8c2777f7cd33a621d36bf189293c5a
Álvaro Sebastián Acosta Cortizas <145587906+alvarosac99@users.noreply.github.com>
2024-11-05 17:24:27 +0100

    Métodos rellenos

--- src/app/menu/menu-principal/menu-principal.component.ts
+++ src/app/menu/menu-principal/menu-principal.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button'; 
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-menu-principal',
@@ -10,5 +12,26 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './menu-principal.component.css'
 })
 export class MenuPrincipalComponent {
+
+  constructor(private router: Router) { }
+
+  goToVersion() {
+    this.router.navigate(['menu/version'])//TABIEN
+  }
+  goToAutores() {
+    this.router.navigate(['menu/autores'])//TABIEN
+  }
+  goToRanking() {
+    this.router.navigate(['menu/ranking'])
+  }
+  goToInformacion() {
+    this.router.navigate(['menu/informacion'])//TABIEN
+  }
+  goToCargar() {
+    this.router.navigate(['menu/cargar'])
+  }
+  goToNueva() {
+    this.router.navigate(['menu/nueva'])
+  }
 
 }
