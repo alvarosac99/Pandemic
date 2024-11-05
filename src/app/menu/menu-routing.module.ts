5fb5bdefb8f16a8b3facb2e674fa13bfb00145d2
Álvaro Sebastián Acosta Cortizas <145587906+alvarosac99@users.noreply.github.com>
2024-11-05 17:44:31 +0100

    EL HUESO
    el hueso el huesito el hueso el huesito el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito  el hueso el huesito

--- src/app/menu/menu-routing.module.ts
+++ src/app/menu/menu-routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { MenuModule } from './menu.module';
 import { MenuPrincipalComponent } from './menu-principal/menu-principal.component';
 import { AutoresComponent } from './autores/autores.component';
+import { VersionComponent } from './version/version.component';
+import { InfoComponent } from './info/info.component';
 
 const routes: Routes = [
   { path: '', component: MenuPrincipalComponent },
-  { path: 'autores', component: AutoresComponent }
+  { path: 'autores', component: AutoresComponent },
+  { path: 'version', component: VersionComponent },
+  { path: 'informacion', component: InfoComponent }
 ];
 
 @NgModule({
