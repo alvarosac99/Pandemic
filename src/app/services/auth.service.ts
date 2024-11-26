e92aeeb0e78c3228e2d852073d6dc7756c1aa214
angel <angelcomu04@gmail.com>
2024-11-26 16:43:47 +0100

    hola sebas

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Usuario } from '../models/usuario.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  register(usuario: Usuario){
+    this.http.post()
+  }
+}
