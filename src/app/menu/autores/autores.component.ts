ce751e3eb0a4aa18f5d1f8d9fe5a8907b4427432
Álvaro Sebastián Acosta Cortizas <145587906+alvarosac99@users.noreply.github.com>
2024-12-02 19:49:57 +0100

    ola Sebas
    así si

--- src/app/menu/autores/autores.component.ts
+++ src/app/menu/autores/autores.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-autores',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './autores.component.html',
   styleUrl: './autores.component.css'
 })
-export class AutoresComponent {
+export class AutoresComponent implements OnInit {
+
+ randomNumber: number = 0;
+
+  ngOnInit(): void {
+    this.randomNumber = Math.floor(Math.random() * 10)
+  }
 
 }
