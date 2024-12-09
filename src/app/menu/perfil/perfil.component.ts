b177951e9a404603614673147ef63af6ae8b7bd8
angel <angelcomu04@gmail.com>
2024-12-09 18:49:18 +0100

    cositas

--- src/app/menu/perfil/perfil.component.ts
+++ src/app/menu/perfil/perfil.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css'
 })
